19ec271e18ac3b3d43c001c47cd2c8b1dd23ebfa
Er <erjoseph25@gmail.com>
2021-09-15 22:19:59 +0800

    Student Acc/Game: Not Yet fixed

--- games.php
+++ games.php
@@ -6,74 +6,61 @@
             <div class="row-fluid">
 				<?php include('games_sidebar.php'); ?>
                 <div class="span9" id="content">
-                     <div class="row-fluid">
+                    <div class="row-fluid">
 					    <!-- breadcrumb -->	
-									<ul class="breadcrumb">
-										<?php
-										$school_year_query = mysqli_query($conn,"select * from school_year order by school_year DESC")or die(mysqli_error());
-										$school_year_query_row = mysqli_fetch_array($school_year_query);
-										$school_year = $school_year_query_row['school_year'];
-										?>
-											<li><a href="#"><b>My Class</b></a><span class="divider">/</span></li>
-										<li><a href="#">School Year: <?php echo $school_year_query_row['school_year']; ?></a><span class="divider">/</span></li>
-										<li><a href="#"><b>Backpack</b></a></li>
-									</ul>
+						 <!-- breadcrumb -->
+					<?php $query = mysqli_query($conn,"SELECT * from teacher_class_student
+					LEFT JOIN teacher_class ON teacher_class.teacher_class_id = teacher_class_student.teacher_class_id 
+					JOIN class ON class.class_id = teacher_class.class_id 
+					JOIN subject ON subject.subject_id = teacher_class.subject_id
+					where student_id = '$session_id'
+					")or die(mysqli_error());
+					$row = mysqli_fetch_array($query);
+					$id = $row['game_id'];
+
+					?>
+					     <ul class="breadcrumb">
+							<li><a href="#"><?php echo $row['class_name']; ?></a> <span class="divider">/</span></li>
+							<li><a href="#"><?php echo $row['subject_code']; ?></a> <span class="divider">/</span></li>
+							<li><a href="#">School Year: <?php echo $row['school_year']; ?></a> <span class="divider">/</span></li>
+							<li><a href="#"><b>Games</b></a></li>
+						</ul>
+						
 						 <!-- end breadcrumb -->
+					 
                         <!-- block -->
                         <div id="block_bg" class="block">
                             <div class="navbar navbar-inner block-header">
-                                <div id="" class="muted pull-right"></div>
+                                <div id="" class="muted pull-left"></div>
                             </div>
                             <div class="block-content collapse in">
                                 <div class="span12">
-							<div class="pull-right">
-								Check All <input type="checkbox"  name="selectAll" id="checkAll" />
-								<script>
-								$("#checkAll").click(function () {
-									$('input:checkbox').not(this).prop('checked', this.checked);
-								});
-								</script>					
-							</div>
-								<?php
-								$query_backpack = mysqli_query($conn,"select * FROM student_backpack where student_id = '$session_id'  order by fdatein DESC ")or die(mysqli_error());
-								$num_row = mysqli_num_rows($query_backpack);
-								if ($num_row > 0){
-								?>
-									<form action="delete_backpack.php" method="post">
-  									<table cellpadding="0" cellspacing="0" border="0" class="table" id="">
-									<a data-toggle="modal" href="#backup_delete" id="delete"  class="btn btn-danger" name=""><i class="icon-trash icon-large"></i></a>
-									<?php include('modal_backpack_delete.php'); ?>
-										<thead>
-										        <tr>
-												<th></th>
-												<th>Date Upload</th>
-												<th>File Name</th>
-												<th>Description</th>
-												<th></th>
-												</tr>
-										</thead>
-										<tbody>
-                              		<?php
-										$query = mysqli_query($conn,"select * FROM student_backpack where student_id = '$session_id'  order by fdatein DESC ")or die(mysqli_error());
-										while($row = mysqli_fetch_array($query)){
-										$id  = $row['file_id'];
-									?>                              
-										<tr id="del<?php echo $id; ?>">
-										<td width="30">
-											<input id="" class="" name="selector[]" type="checkbox" value="<?php echo $id; ?>">
-										</td>
-										 <td><?php echo $row['fdatein']; ?></td>
-                                         <td><?php  echo $row['fname']; ?></td>
-                                         <td><?php echo $row['fdesc']; ?></td>                                      
-                                         <td width="30"><a href="<?php echo $row['floc']; ?>"><i class="icon-download icon-large"></i></a></td>                                      
-										</tr>
+									<ul	 id="da-thumbs" class="da-thumbs">
+										    <?php
+										 
+										 
+														$my_student = mysqli_query($conn,"SELECT *
+														FROM teacher_class_student
+														LEFT JOIN student ON student.student_id = teacher_class_student.student_id
+														INNER JOIN class ON class.class_id = student.class_id where teacher_class_id = '$get_id' order by lastname ")or die(mysqli_error());
+														
+														while($row = mysqli_fetch_array($my_student)){
+														$id = $row['teacher_class_student_id'];
+														?>
+														
+											<li id="del<?php echo $id; ?>">
+												<a  class="classmate_cursor" href="#">
+														<img id="student_avatar_class" src ="admin/<?php echo $row['location'] ?>" width="124" height="140" class="img-polaroid">
+													<div><span></span></div>
+												</a>
+												<p class="class"><?php echo $row['lastname'];?></p>
+												<p class="subject"><?php echo $row['firstname']; ?></p>
+											</li>
 									<?php } ?>
-										</tbody>
-									</table>
-									</form>
-									<?php }else{ ?>
-									<div class="alert alert-info"><i class="icon-info-sign"></i> No Files Inside Your Games.</div>
-									<?php } ?>
+									</ul>
+                            </tbody>
+                        </table>
+						
                                 </div>
                             </div>
                         </div>
